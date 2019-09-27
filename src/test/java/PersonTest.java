7586a8e13335c2f0bd7e12c2340906e1d499317f
alyonagerasimova <alena.gerasimova1999@gmail.com>
2019-09-28 03:13:43 +0400

    Task 1.10

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void testPerson() {
+
+        Person person1 = new Person();
+        person1.setFirstName("Алексей");
+        person1.setLastName("Никитин");
+        person1.setPassportId(308748323);
+
+        String result = "Алексей";
+        String result1 = "Никитин";
+        int result2 = 308748323;
+
+        assertEquals(person1.getFirstName(), result);
+        assertEquals(person1.getLastName(), result1);
+        assertEquals(person1.getPassportId(), result2);
+
+
+    }
+
+}
