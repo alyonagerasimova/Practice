5a2a34508aa01e542ff0b91064ab9c5986314241
alyonagerasimova <alena.gerasimova1999@gmail.com>
2019-12-21 22:52:09 +0400

    Task 1.9, 1.11

--- src/test/java/ru/ssau/tk/gropprjone/practice/PersonTest.java
+++ src/test/java/ru/ssau/tk/gropprjone/practice/PersonTest.java
@@ -1,6 +1,7 @@
 package ru.ssau.tk.gropprjone.practice;
 
 import org.junit.jupiter.api.Test;
+
 import static org.testng.Assert.*;
 
 class PersonTest {
@@ -22,9 +23,53 @@ class PersonTest {
         assertEquals(person1.getFirstName(), result);
         assertEquals(person1.getLastName(), result1);
         assertEquals(person1.getPassportId(), result2);
-        assertEquals(person1.getGender(),male);
-
-
+        assertEquals(person1.getGender(), male);
     }
 
+    @Test
+    void passportIdTest() {
+        int testValue = 4;
+         Person person1 = new Person();
+         Person person2 = new Person("","");
+         Person person3 = new Person(testValue);
+
+         person1.setPassportId(testValue);
+         person2.setPassportId(testValue);
+
+         assertEquals(person1.getPassportId(), testValue);
+         assertEquals(person2.getPassportId(), testValue);
+         assertEquals(person3.getPassportId(), testValue);
+    }
+
+    @Test
+    void firstNameTest() {
+        String testValue = "value";
+        Person person1 = new Person();
+        Person person2 = new Person(testValue,"");
+        Person person3 = new Person(null,"");
+        Person person4 = new Person();
+
+        person1.setFirstName(testValue);
+
+        assertEquals(person1.getFirstName(), testValue);
+        assertEquals(person2.getFirstName(), testValue);
+        assertNull(person3.getFirstName());
+        assertNull(person4.getFirstName());
+    }
+
+    @Test
+    void lastNameTest(){
+        String testValue = "value";
+        Person person1 = new Person();
+        Person person2 = new Person("",testValue);
+        Person person3 = new Person("",null);
+        Person person4 = new Person();
+
+        person1.setLastName(testValue);
+
+        assertEquals(person1.getLastName(), testValue);
+        assertEquals(person2.getLastName(), testValue);
+        assertNull(person3.getLastName());
+        assertNull(person4.getLastName());
+    }
 }
