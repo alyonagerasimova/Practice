704f4efb03c6e0e60f63561eb37bc4de07aff773
alyonagerasimova <alena.gerasimova1999@gmail.com>
2019-12-21 23:05:34 +0400

    Task 1.13,1.14

--- src/main/java/ru/ssau/tk/gropprjone/practice/Program.java
+++ src/main/java/ru/ssau/tk/gropprjone/practice/Program.java
@@ -1,8 +1,9 @@
 package ru.ssau.tk.gropprjone.practice;
 
+
 public class Program {
-  /*  public static void main(String[] args) {
-        ru.ssau.tk.Practice.Tasks.functions.Person person = new ru.ssau.tk.Practice.Tasks.functions.Person();// Ссылка, указвает на обьект в куче
+    public static void main(String[] args) {
+       Person person = new Person();// Ссылка, указвает на обьект в куче
         person.setFirstName("Arkadiy");
         int number = 5; // Хранится в стеке, передается по значению
         checkInt(number);
@@ -11,16 +12,16 @@ public class Program {
         System.out.println(person.getFirstName());
     }
 
-    private static void checkInt(int number) {// Т.к простоц тип по значению
+    private static void checkInt(int number) {// Т.к простой тип по значению
         number = 10;
         System.out.println(number);
     }
 
-    private static void checkPerson(ru.ssau.tk.Practice.Tasks.functions.Person person) {// Передается ссылка на обьект в куче
+    private static void checkPerson(Person person) {// Передается ссылка на обьект в куче
         person.setFirstName("Oleg");// изменение обьекта
         System.out.println(person.getFirstName());
-    }*/
-  public static void main(String[] args) {
+    }
+ /* public static void main(String[] args) {
       Person person = new Person();
       person.setFirstName("Arkadiy");
       checkAnotherPerson(person);
@@ -33,4 +34,7 @@ public class Program {
         System.out.println(person.getFirstName());
     }
 
+  */
+//Сначала выводится Ignat , т.к вначале вызывается метод, который иниациализирует имя и выводит его в консоль, после этого
+//производится вывод в консоль имени person, заданном в  main
 }
